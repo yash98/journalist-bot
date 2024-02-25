62c551eb5646192de9ab57559793200054d56e92
Yash Malviya <yash.malviya1@myntra.com>
2024-02-25 15:36:31 +0530

    basic adaptation of frontend for survery

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,57 +1,20 @@
 import streamlit as st
+from chat import chat
+# import streamlit_debug
+# streamlit_debug.set(flag=True, wait_for_client=True, host='localhost', port=8765)
 
+def state_values_exists_eq(key, value):
+    return key in st.session_state and st.session_state[key] == value
 
-STARTING_MSG = "What topic would you describe today?"
+def login():
+    if not state_values_exists_eq("survey_started", True):
+        st.title("Dynamic Survey")
+        st.session_state.name = st.text_input("Enter your email:")
+        st.session_state.form_id = st.text_input("Enter form id:")
 
-# App title
-st.set_page_config(page_title="Arnab : Journalist Bot")
+    if state_values_exists_eq("survey_started", True) or st.button("Start Survey"):
+        st.session_state.survey_started = True
+        chat()
 
-# Replicate Credentials
-with st.sidebar:
-    st.title('Arnab : Your Friendly Neighbourhood Journalist Bot')
-
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": STARTING_MSG}]
-
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": STARTING_MSG}]
-# st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
-
-def generate_bot_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
-    for dict_message in st.session_state.messages:
-        if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
-        else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
-    # output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
-    #                        input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-    #                               "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
-    output = ""
-    return output
-
-# User-provided prompt
-if prompt := st.chat_input("Write here"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = generate_bot_response(prompt)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+if __name__ == "__main__":
+    login()
