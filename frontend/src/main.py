bf18cb20bc063ea856d758a5344ba5bae376750e
Yash Malviya <300073166@8735-300073166l.mynt.myntra.com>
2024-03-02 16:56:53 +0530

    session email correct

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -9,7 +9,7 @@ def state_values_exists_eq(key, value):
 def login():
     if not state_values_exists_eq("survey_started", True):
         st.title("Dynamic Survey")
-        st.session_state.name = st.text_input("Enter your email:")
+        st.session_state.email = st.text_input("Enter your email:")
         st.session_state.form_id = st.text_input("Enter form id:")
 
     if state_values_exists_eq("survey_started", True) or st.button("Start Survey"):
