8346f79681832d977ff95501bcefe5d241eb5246
Yash Malviya <yash.malviya1@myntra.com>
2024-03-09 17:51:17 +0530

    basic ui

--- frontend/src/survey_creator.py
+++ frontend/src/survey_creator.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import requests
+
+BACKEND_URL = "http://localhost:8080"
 
 USER_INPUT_KEY = "user_inputs"
 
 QUESTION_KEY = "question"
 OBJECTIVE_KEY = "objective"
+FOLLOWUP_DEPTH_KEY = "followup_depth"
 
 def add_empty_question():
     st.session_state[USER_INPUT_KEY].append({QUESTION_KEY: "", OBJECTIVE_KEY: [""]})
@@ -35,8 +39,38 @@ def update_user_input_based_on_session_keys():
             id_o = int(key.split("_")[2])
             st.session_state[USER_INPUT_KEY][id_q][OBJECTIVE_KEY][id_o] = st.session_state[key]
 
-def survey():
-    # Initialize an empty list to store user inputs
+"""
+curl -X POST -H "Content-Type: application/json" \
+    -d '{"form_id":1001, "questions":[{"question":"How would you rate the availability and affordability of healthcare services in your rural area?","question_config":{"followup_depth":1,"criteria":["The answer should have a perception healthcare availability in rural areas"]}},{"question":"Have you experienced any difficulties in accessing healthcare facilities due to distance or transportation issues?","question_config":{"followup_depth":3,"criteria":["To evaluate the impact of distance and transportation on healthcare access in rural areas","To identify areas where transportation infrastructure or services need improvement to enhance healthcare accessibility"]}},{"question":"How satisfied are you with the quality of healthcare services, including the availability of medical professionals and equipment, in your rural area?","question_config":{"followup_depth":3,"criteria":["To gauge satisfaction levels with the quality of healthcare services, identifying areas for improvement","To assess the availability of medical professionals and equipment, highlighting any deficiencies in healthcare infrastructure"]}}]}' \
+    http://localhost:8080/store_data/
+"""
+def make_submit_survey_call():
+    # check if empty string in question or objective and exclude from new json payload object it before sending
+    questions_list = [] 
+    for input_data in st.session_state[USER_INPUT_KEY]:
+        question_data = {}
+        if input_data[QUESTION_KEY] == "":
+            continue
+
+        question_data["question"] = input_data[QUESTION_KEY]
+        question_data["question_config"] = {}
+
+        for objective in input_data[OBJECTIVE_KEY]:
+            if objective != "":
+                if "criteria" not in question_data["question_config"]:
+                    question_data["question_config"]["criteria"] = []
+                question_data["question_config"]["criteria"].append(objective)
+
+        questions_list.append(question_data)
+
+    data = {
+        "form_id": None,
+        "questions": questions_list
+    }
+    response = requests.post(BACKEND_URL+"/store_data/", json=data)
+    return response
+
+def survey_creator():
     if USER_INPUT_KEY not in st.session_state:
         st.session_state[USER_INPUT_KEY] = []
         add_empty_question()
@@ -48,4 +82,4 @@ def survey():
         update_user_input_based_on_session_keys()
         st.write(st.session_state[USER_INPUT_KEY])
 
-survey()
+survey_creator()
