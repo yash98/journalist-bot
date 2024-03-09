c4af5e8f7a5337e7952ae227cd0a938c05d57602
Yash Malviya <yashmalviya98@gmail.com>
2024-03-09 15:14:27 +0530

    Adapt survey bot to new agent interface (#5)
    * network call and dependencies
    
    * skeleton ready
    
    * adapt bot to new interface, yet to be tested
    
    * clear history + first message functionality
    
    * session email correct
    
    * query param correction
    
    * debug
    
    * type corrections and self
    
    * debug logs
    
    * bug
    
    * fix
    
    * hot reload streamlit
    
    * fix
    
    * transform chat history
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * correct completion
    
    * init
    
    * spinner and error fix
    
    * fix
    
    * fix
    
    * fix
    
    * changes
    
    * documentation and dependencies
    
    ---------
    
    Co-authored-by: Yash Malviya <yash.malviya1@myntra.com>
    Co-authored-by: Yash Malviya <300073166@8735-300073166l.mynt.myntra.com>

---
+++ backend/README.md
@@ -0,0 +1,7 @@
+# How to run
+
+```sh
+cd backend
+pip3 install -R requirements.txt
+uvicorn 
+```
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from survey_bot_v1 import SurveyBotV1
+from request import Question
+from typing import List, Optional
+from response import HistoryMessage
+import logging
 
 app = FastAPI()
-
-class UserRequest(BaseModel):
-	email: str
-	form_id: int
-	user_answer: str
 
 # Temporary persistence work around
 survey_store = {}
@@ -15,17 +14,13 @@ survey_store = {}
 # form_id -> fixed questions
 fixed_questions_store = {}
 
-class QuestionConfig(BaseModel):
-	followup_depth: int
-	criteria: List[str]
-
-class Question(BaseModel):
-	question: str
-	question_config: QuestionConfig
-
 class FormRequest(BaseModel):
 	form_id : int
 	questions : List[Question]
+
+class FollowUpResponse(BaseModel):
+	next_question: Optional[str]
+	status: str
 
 @app.post("/store_data/")
 async def store_data(formRequest: FormRequest):
@@ -34,6 +29,18 @@ async def store_data(formRequest: FormRequest):
 	fixed_questions_store[key] = value
 	# print("Current value of dictionary : ", fixed_questions_store)
 	return {"message": "Data stored successfully"}
+
+class UserRequest(BaseModel):
+	email: str
+	form_id: int
+	user_answer: str
+
+def create_new_survey_bot(email: str, form_id: int):
+	if form_id in fixed_questions_store:
+		survey_bot = SurveyBotV1(fixed_questions_store[form_id])
+		survey_store[(email, form_id)] = survey_bot
+		return survey_bot
+	return None
 
 @app.post("/user/get_next_question")
 async def generate_follow_up(userRequest: UserRequest):
@@ -45,12 +52,37 @@ async def generate_follow_up(userRequest: UserRequest):
 		if (email, form_id) in survey_store:
 			survey_bot = survey_store[(email, form_id)]
 		else:
-			if form_id in fixed_questions_store:
-				survey_bot = SurveyBotV1(fixed_questions_store[form_id])
-				survey_store[(email, form_id)] = survey_bot
-			else:
-				raise HTTPException(status_code=404, detail="Form ID not found")
-		next_question = survey_bot.get_next_question(user_answer)
-		return {"next_question": next_question}
+			raise HTTPException(status_code=404, detail="Survey bot for Email, Form ID pair not found")
+		(next_question, state) = survey_bot.get_next_question(user_answer)
+		return FollowUpResponse(next_question=next_question, status=state)
 	except Exception as e:
+		logging.exception("/user/get_next_question userRequest: " + str(userRequest) + " error: " + str(e))
+		raise HTTPException(status_code=500, detail=str(e))
+
+# Get API takes email and form_id as input and deletes the survey_bot object from the survey_store
+@app.get("/user/clear_history")
+async def clear_history(email: str, form_id: int):
+	try:
+		if (email, form_id) in survey_store:
+			del survey_store[(email, form_id)]
+			return {"message": "History cleared successfully"}
+		return {"message": "No history found for given email and form_id"}
+	except Exception as e:
+		logging.exception("/user/clear_history failed email: " + email + " form_id: " + str(form_id) + " error: " + str(e))
+		raise HTTPException(status_code=500, detail=str(e))
+
+# Get API takes email and form_id as input and returns the survey_bot object from the survey_store
+@app.get("/user/get_history")
+async def get_history(email: str, form_id: int) -> List[HistoryMessage]:
+	try:
+		if (email, form_id) in survey_store:
+			return survey_store[(email, form_id)].get_chat_history()
+		else:
+			survey_bot = create_new_survey_bot(email, form_id)
+			if survey_bot:
+				return survey_bot.get_chat_history()
+			else:
+				raise HTTPException(status_code=404, detail="Survey bot for Email, Form ID pair not found")
+	except Exception as e:
+		logging.exception("/user/get_history failed email: " + email + " form_id: " + str(form_id) + " error: " + str(e))
 		raise HTTPException(status_code=500, detail=str(e))
